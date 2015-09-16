325fa5951e41399805ec859c4c5a10b9cb092bcf
River Merrill <rivermerrill.me@gmail.com>
2015-09-16 14:00:23 -0600

    Added dropdown menu to select position

--- app.js
+++ app.js
@@ -3,20 +3,27 @@ var Player = function (name, position, num) {
 	this.position = position;
 	this.num = num;
 }
-
+var addToDropdown = function(value){
+$(document.createElement('option')).val(value).text(value).appendTo($('.dropdown-selector'));
+};
 function addToRoster(element, classAdd, text, append) {
 	$(document.createElement(element)).addClass(classAdd).text(text).appendTo(append);
 }
 
+addToDropdown('Center');
+addToDropdown('Offensive Guard');
+addToDropdown('Offensive Tackle');
+addToDropdown('Quarterback');
+addToDropdown('Running Back');
+addToDropdown('Wide Receiver');
+addToDropdown('Tight end');
+addToDropdown('Defensive Tackle');
+addToDropdown('Devensive End');
+addToDropdown('Middle Linebacker');
+addToDropdown('Outside Linebacker');
+addToDropdown('Cornerback');
+addToDropdown('Safety');
 
-
-$('#buttonAdd').attr('disabled', true);
-$(document).ready(function () {
-	if ($(this).val().length != 0)
-		$('#buttonAdd').attr('disabled', false);
-	else
-		$('#buttonAdd').attr('disabled', true);
-});
 
 
 $('#buttonAdd').on('click', function () {
@@ -25,7 +32,7 @@ $('#buttonAdd').on('click', function () {
 	var playerNumber = $('#playerNumber').val();
 	var newPlayer = new Player(playerName, playerPosition, playerNumber);
 	$('<div></div>').appendTo('.player-roster').addClass('player-card').on('click', function () {
-		$(this).toggleClass('selected').animate({ top: '10px' }, 300);
+		$(this).toggleClass('selected');
 	});
 	var dropdownSelector = $('.dropdown-selector').val();
 	$(document.createElement('img')).attr('src', 'http://2f13yq12csmv2yraq925m73i.wpengine.netdna-cdn.com/wp-content/uploads/2011/08/boise-state-uga-game.jpg').fadeIn('500').appendTo('div .player-card:last-child');
