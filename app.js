231b955828e6115037184d43eaa949696f4b43f2
River Merrill <rivermerrill.me@gmail.com>
2015-09-18 00:34:10 -0600

    Fixed bugs and removed unneeded code.

--- app.js
+++ app.js
@@ -3,9 +3,6 @@ var Player = function (name, position, num) {
 	this.position = position;
 	this.num = num;
 }
-var addToDropdown = function (value) {
-	$(document.createElement('option')).val(value).text(value).appendTo($('.dropdown-selector'));
-};
 function addToRoster(element, classAdd, text, append) {
 	$(document.createElement(element)).addClass(classAdd).text(text).appendTo(append);
 }
@@ -15,9 +12,6 @@ var apiUrl = url + encodeURIComponent(url2);
 
 $('#buttonAdd').on('click', function () {
 	var playerName = $('#playerName').val();
-	var playerPosition = $('#playerPosition').val();
-	var playerNumber = $('#playerNumber').val();
-	var newPlayer = new Player(playerName, playerPosition, playerNumber);
 	$('<div></div>').appendTo('.player-roster').addClass('player-card').on('click', function () {
 		$(this).toggleClass('selected');
 	});
@@ -25,20 +19,16 @@ $('#buttonAdd').on('click', function () {
 		var players = data.body.players;
 		players.forEach(function (player) {
 			if (player.fullname === playerName) {
+				addToRoster('h3','PlayerName',player.fullname,'div .player-card:last-child')
 				$(document.createElement('img')).attr('src', player.photo).fadeIn('500').appendTo('div .player-card:last-child');
-				$(document.createElement('h3')).text(player.position).addClass('PlayerPosition').appendTo('div .player-card:last-child')
+				addToRoster('h3', 'PlayerPosition', player.position, 'div .player-card:last-child');
 				addToRoster('h3', 'PlayerNumber', '#' + player.jersey, 'div .player-card:last-child');
 			}
 
 		});
 	})
-	var dropdownSelector = $('.dropdown-selector').val();
-	addToRoster('h3', 'PlayerName', newPlayer.name, 'div .player-card:last-child')
-	addToRoster('h3', 'PlayerNumber', newPlayer.num, 'div .player-card:last-child');
 	$('#playerName, #playerPosition, #playerNumber').val('');
 });
-
-$('.player-card').draggable();
 
 $('#buttonRemove').on('click', function () {
 	$('.selected').fadeOut('slow');
