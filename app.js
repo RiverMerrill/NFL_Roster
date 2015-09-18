ce98a2bff13b666f1d0a793831fa1673b05c36fc
River Merrill <rivermerrill.me@gmail.com>
2015-09-18 00:00:59 -0600

    Finished All Assignments - Added NFL API - Used Jquery UI Autocomplete

--- app.js
+++ app.js
@@ -3,28 +3,15 @@ var Player = function (name, position, num) {
 	this.position = position;
 	this.num = num;
 }
-var addToDropdown = function(value){
-$(document.createElement('option')).val(value).text(value).appendTo($('.dropdown-selector'));
+var addToDropdown = function (value) {
+	$(document.createElement('option')).val(value).text(value).appendTo($('.dropdown-selector'));
 };
 function addToRoster(element, classAdd, text, append) {
 	$(document.createElement(element)).addClass(classAdd).text(text).appendTo(append);
 }
-
-addToDropdown('Center');
-addToDropdown('Offensive Guard');
-addToDropdown('Offensive Tackle');
-addToDropdown('Quarterback');
-addToDropdown('Running Back');
-addToDropdown('Wide Receiver');
-addToDropdown('Tight end');
-addToDropdown('Defensive Tackle');
-addToDropdown('Devensive End');
-addToDropdown('Middle Linebacker');
-addToDropdown('Outside Linebacker');
-addToDropdown('Cornerback');
-addToDropdown('Safety');
-
-
+var url = "http://bcw-getter.herokuapp.com/?url=";
+var url2 = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
+var apiUrl = url + encodeURIComponent(url2);
 
 $('#buttonAdd').on('click', function () {
 	var playerName = $('#playerName').val();
@@ -34,14 +21,36 @@ $('#buttonAdd').on('click', function () {
 	$('<div></div>').appendTo('.player-roster').addClass('player-card').on('click', function () {
 		$(this).toggleClass('selected');
 	});
+	$.getJSON(apiUrl, function (data) {
+		var players = data.body.players;
+		players.forEach(function (player) {
+			if (player.fullname === playerName) {
+				$(document.createElement('img')).attr('src', player.photo).fadeIn('500').appendTo('div .player-card:last-child');
+				$(document.createElement('h3')).text(player.position).addClass('PlayerPosition').appendTo('div .player-card:last-child')
+				addToRoster('h3', 'PlayerNumber', '#' + player.jersey, 'div .player-card:last-child');
+			}
+
+		});
+	})
 	var dropdownSelector = $('.dropdown-selector').val();
-	$(document.createElement('img')).attr('src', 'http://2f13yq12csmv2yraq925m73i.wpengine.netdna-cdn.com/wp-content/uploads/2011/08/boise-state-uga-game.jpg').fadeIn('500').appendTo('div .player-card:last-child');
 	addToRoster('h3', 'PlayerName', newPlayer.name, 'div .player-card:last-child')
-	$(document.createElement('h3')).addClass('PlayerPosition').appendTo('div .player-card:last-child').append(dropdownSelector)
 	addToRoster('h3', 'PlayerNumber', newPlayer.num, 'div .player-card:last-child');
 	$('#playerName, #playerPosition, #playerNumber').val('');
 });
 
+$('.player-card').draggable();
+
 $('#buttonRemove').on('click', function () {
-	$('.selected').fadeOut();
+	$('.selected').fadeOut('slow');
+});
+
+$.getJSON(apiUrl, function (data) {
+	var players = data.body.players;
+	var playerArr = [];
+	players.forEach(function (player) {
+		playerArr.push(player.fullname);
+	});
+	$("#playerName").autocomplete({
+		source: playerArr
+	});
 });
