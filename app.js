9ef816cfa0d6370ee126b47037c718f41c428616
River Merrill <rivermerrill.me@gmail.com>
2015-09-19 22:27:46 -0600

    Removed secondary JSON request, player photos not visible.

--- app.js
+++ app.js
@@ -9,24 +9,31 @@ function addToRoster(element, classAdd, text, append) {
 var url = "http://bcw-getter.herokuapp.com/?url=";
 var url2 = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
 var apiUrl = url + encodeURIComponent(url2);
+var playerPhotos = [];
+var playerNames = [];
+$.getJSON(apiUrl, function (data) {
+	var players = data.body.players;
+	players.forEach(function (player) {
+		playerNames.push(player.fullname);
+		playerPhotos.push(players.photo);
+	});
+	$("#playerName").autocomplete({
+		source: playerNames
+	});
+});
 
 $('#buttonAdd').on('click', function () {
 	var playerName = $('#playerName').val();
 	$('<div></div>').appendTo('.player-roster').addClass('player-card').on('click', function () {
 		$(this).toggleClass('selected');
 	});
-	$.getJSON(apiUrl, function (data) {
-		var players = data.body.players;
-		players.forEach(function (player) {
-			if (player.fullname === playerName) {
-				addToRoster('h3','PlayerName',player.fullname,'div .player-card:last-child')
-				$(document.createElement('img')).attr('src', player.photo).fadeIn('500').appendTo('div .player-card:last-child');
-				addToRoster('h3', 'PlayerPosition', player.position, 'div .player-card:last-child');
-				addToRoster('h3', 'PlayerNumber', '#' + player.jersey, 'div .player-card:last-child');
-			}
+	for (var i = 0; i < playerNames.length; i++) {
+		if (playerNames[i] === playerName) {
+			addToRoster('h3', 'PlayerName', playerNames[i], 'div .player-card:last-child')
+			$(document.createElement('img')).attr('src', playerPhotos[i]).fadeIn('500').appendTo('div .player-card:last-child');
+		}
 
-		});
-	})
+	};
 	$('#playerName, #playerPosition, #playerNumber').val('');
 });
 
@@ -34,13 +41,13 @@ $('#buttonRemove').on('click', function () {
 	$('.selected').fadeOut('slow');
 });
 
-$.getJSON(apiUrl, function (data) {
-	var players = data.body.players;
-	var playerArr = [];
-	players.forEach(function (player) {
-		playerArr.push(player.fullname);
-	});
-	$("#playerName").autocomplete({
-		source: playerArr
-	});
-});
+// $.getJSON(apiUrl, function (data) {
+// 	var players = data.body.players;
+// 	var playerArr = [];
+// 	players.forEach(function (player) {
+// 		playerArr.push(player.fullname);
+// 	});
+// 	$("#playerName").autocomplete({
+// 		source: playerArr
+// 	});
+// });
