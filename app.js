b8831cc8c1aab5a7f0a1bd1c8fc5b9f87c02817b
River Merrill <rivermerrill.me@gmail.com>
2015-09-21 08:59:03 -0600

    SyntaxError

--- app.js
+++ app.js
@@ -1,36 +1,49 @@
-var Player = function (name, position, num) {
-	this.name = name;
-	this.position = position;
-	this.num = num;
-}
 function addToRoster(element, classAdd, text, append) {
 	$(document.createElement(element)).addClass(classAdd).text(text).appendTo(append);
 }
 var url = "http://bcw-getter.herokuapp.com/?url=";
 var url2 = "http://api.cbssports.com/fantasy/players/list?version=3.0&SPORT=football&response_format=json";
 var apiUrl = url + encodeURIComponent(url2);
-var playerPhotos = [];
-var playerNames = [];
-$.getJSON(apiUrl, function (data) {
-	var players = data.body.players;
-	players.forEach(function (player) {
-		playerNames.push(player.fullname);
-		playerPhotos.push(players.photo);
-	});
-	$("#playerName").autocomplete({
-		source: playerNames
-	});
-});
+var players = {};
+​
+//pass True if you want to force a reload of the data from the API.
+function loadPlayerData(forceReload) {
+	var cache = JSON.parse(localStorage.getItem("playerData"));
+	if (cache && !forceReload) {
+		players = cache;
+	} else {
+		$.getJSON(apiUrl, function (data) {
+			players = data.body.players;
+			localStorage.setItem("playerData", JSON.stringify(players));
+			$("#playerName").autocomplete({
+				source: players.fullname
+			});
+		});
+	}
+}
+
+loadPlayerData();
+// Old code
+// $.getJSON(apiUrl, function (data) {
+// 	var players = data.body.players;
+// 	players.forEach(function (player) {
+// 		playerNames.push(player.fullname);
+// 		playerPhotos.push(player.photo);
+// 	});
+// 	$("#playerName").autocomplete({
+// 		source: playerNames
+// 	});
+// });
 
 $('#buttonAdd').on('click', function () {
 	var playerName = $('#playerName').val();
 	$('<div></div>').appendTo('.player-roster').addClass('player-card').on('click', function () {
 		$(this).toggleClass('selected');
 	});
-	for (var i = 0; i < playerNames.length; i++) {
-		if (playerNames[i] === playerName) {
-			addToRoster('h3', 'PlayerName', playerNames[i], 'div .player-card:last-child')
-			$(document.createElement('img')).attr('src', playerPhotos[i]).fadeIn('500').appendTo('div .player-card:last-child');
+	for (var i = 0; i < players.length; i++) {
+		if (players.fullname[i] === playerName) {
+			addToRoster('h3', 'PlayerName', players.fullname[i], 'div .player-card:last-child')
+			$(document.createElement('img')).attr('src', players.photo[i]).fadeIn('500').appendTo('div .player-card:last-child');
 		}
 
 	};
@@ -41,6 +54,7 @@ $('#buttonRemove').on('click', function () {
 	$('.selected').fadeOut('slow');
 });
 
+// Old code
 // $.getJSON(apiUrl, function (data) {
 // 	var players = data.body.players;
 // 	var playerArr = [];
